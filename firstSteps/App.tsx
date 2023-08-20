fbc4309bd202f5f40aec896ece142195898961f1
ruedaKaff <jhanru@live.com>
2023-08-20 12:53:09 -0500

    FirstSteps RN, setups ordening

---
+++ firstSteps/App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Hola mundo !!!</Text>
+    </View>
+  )
+}
+
+export default App;
